0b667a8c9df78ccb33c612927e8eb7155060e7f5
fflogin <150805466+fflogin@users.noreply.github.com>
2023-11-14 14:44:02 +0200

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC_xdW020eVvL9rHxjT4f8q6TUpwLNzOZc",
+  authDomain: "my-2nd-project-191ba.firebaseapp.com",
+  projectId: "my-2nd-project-191ba",
+  storageBucket: "my-2nd-project-191ba.appspot.com",
+  messagingSenderId: "725890826408",
+  appId: "1:725890826408:web:d84e95a052231602fe029d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
